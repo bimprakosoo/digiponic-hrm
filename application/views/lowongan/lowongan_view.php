e8e3e85ca40614536c2782a0f6cd682975a1cf96
aprildapeter <75058126+aprildapeter@users.noreply.github.com>
2021-09-27 08:32:36 +0700

    update-navbar+css

--- application/views/lowongan/lowongan_view.php
+++ application/views/lowongan/lowongan_view.php
@@ -19,45 +19,44 @@
   <body>
        <!-- Awal menu -->
        <header class=body-warp>
-        <nav class="navbar navbar-expand-lg navbar-light fixed-top">
+       <nav class="navbar navbar-expand-lg navbar-light fixed-top">
             <div class="container">
-                <a class="navbar-brand" href="<?php echo base_url();?>"><img src="<?php echo base_url();?>/assets/image/Logo.svg" alt=""></a>
-                      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
-                         </button>
+                <a class="navbar-brand" href="<?php echo base_url(); ?>"><img src="<?php echo base_url(); ?>/assets/image/Logo.svg" alt=""></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <ul class="navbar-nav mr-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="<?php echo base_url();?>LandingPage">Home</a>
+                            <a class="nav-link active" aria-current="page" href="<?php echo base_url(); ?>LandingPage">Home</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="<?php echo base_url();?>Tentang/tentang">Tentang</a>
+                            <a class="nav-link" href="<?php echo base_url(); ?>Tentang/tentang">Tentang</a>
                         </li>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Departemen
+                                Departemen
                             </a>
                             <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="#">Departemen</a></li>
+                                <li><a class="dropdown-item" href="#">Departemen</a></li>
                                 <li><a class="dropdown-item" href="#">Karyawan</a></li>
                             </ul>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="<?php echo base_url();?>LandingPage/Lowongan">Lowongan</a>
+                            <a class="nav-link" href="<?php echo base_url(); ?>LandingPage/Lowongan">Lowongan</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="<?php echo base_url();?>LandingPage/artikel">Artikel</a>
+                            <a class="nav-link" href="<?php echo base_url(); ?>LandingPage/artikel">Artikel</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="<?php echo base_url();?>LandingPage/kontak">Kontak</a>
+                            <a class="nav-link" href="<?php echo base_url(); ?>LandingPage/kontak">Kontak</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="btn btn-secondary" href="<?php echo base_url();?>login">Login</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="btn btn-primary" href="<?php echo base_url();?>register">Register</a>
-                        </li>
-                        </ul>
+
+                    </ul>
+                    <div class="gap-3 ms-auto">
+                        <a class="btn btn-outline-light" href="<?php echo base_url(); ?>login">Login</a>
+                        <a class="btn btn-success" href="<?php echo base_url(); ?>register">Register</a>
+                    </div>
                 </div>
             </div>
         </nav>
