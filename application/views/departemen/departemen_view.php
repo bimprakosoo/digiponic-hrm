16dd2cb88f75d5323d7f5a4ed5228405839773fe
aprildapeter <75058126+aprildapeter@users.noreply.github.com>
2021-09-27 20:37:20 +0700

    membuat-view-departemen

--- application/views/departemen/departemen_view.php
+++ application/views/departemen/departemen_view.php
@@ -8,12 +8,13 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="<?php echo base_url();?>assets/css/bootstrap.css" />
     <link rel="stylesheet" href="<?php echo base_url();?>assets/css/custom.css" />
-    
+
     <!-- Font -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
     
+  
   </head>
   <body>
        <!-- Awal menu -->
@@ -60,34 +61,16 @@
             </div>
         </nav>
     <header>
-      <div class="slider w-100">
-        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-          <div class="carousel-indicators">
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+
+     <!-- Awal menu -->
+     <section class="departemen" id="departemen">
+          <img src="<?php echo base_url();?>assets/image/departemen/departemen.svg" class="d-block w-100" />
+          <div class="text-center">
+              <h4>Departemen</h4>
           </div>
-          <div class="carousel-inner">
-            <div class="carousel-item active">
-              <img src="<?php echo base_url();?>/assets/image/slide/slide-1.jpg" class="d-block w-100" alt="...">
-            </div>
-            <div class="carousel-item">
-              <img src="<?php echo base_url();?>/assets/image/slide/slide-2.jpg" class="d-block w-100" alt="...">
-            </div>
-            <div class="carousel-item">
-              <img src="<?php echo base_url();?>/assets/image/slide/slide-3.jpg" class="d-block w-100" alt="...">
-            </div>
-          </div>
-          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-          </button>
-          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-          </button>
-        </div>
-        </div>
+        
+      <section>
+<!-- Akhir menu -->
     <footer id="footer">
       <div class="container-fluid">
         <div class="copyright">
