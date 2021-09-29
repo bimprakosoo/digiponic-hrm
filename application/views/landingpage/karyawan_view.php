523210acce7f0f8026f5b2e83d49f7e8915c2322
aprildapeter <75058126+aprildapeter@users.noreply.github.com>
2021-09-29 21:21:41 +0700

    coba css

--- application/views/landingpage/karyawan_view.php
+++ application/views/landingpage/karyawan_view.php
@@ -139,9 +139,9 @@
                         Operation
                         </div>
                     </div>
-                            <a href="#" class="btn btn-primary">
+                            <button type="button" data-bs-toggle="modal" data-bs-target="#exampleModal" class="btn btn-primary">
                                 Lihat Selengkapnya
-                            </a>
+                            </button>
                 </div>
                 </div>
             </div>
@@ -433,6 +433,42 @@
       </div>
     </footer>
 
+    <!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  Launch demo modal
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Karyawan</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+      <div class="card mb-3" style="max-width: 540px;">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="<?php echo base();?>assets/image/karyawan/karyawan-foto/svg" class="img-fluid rounded-start" alt="...">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
     <script src="<?php echo base_url();?>/assets/js/bootstrap.min.js"></script>
   </body>
 </html>
