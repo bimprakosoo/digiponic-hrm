51b1e667088acc000602698aac8c994603f90767
ullum <ulumw18@gmail.com>
2021-10-18 09:21:26 +0700

    Update digiponic-hrm.sql

--- sql/digiponic-hrm.sql
+++ sql/digiponic-hrm.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 04, 2021 at 03:47 AM
+-- Generation Time: Oct 18, 2021 at 04:20 AM
 -- Server version: 10.4.20-MariaDB
 -- PHP Version: 8.0.8
 
@@ -48,7 +48,39 @@ CREATE TABLE `data_lamaran` (
 --
 
 INSERT INTO `data_lamaran` (`id`, `nama`, `provinsi`, `kota_kabupaten`, `kecamatan`, `alamat_lengkap`, `jk`, `tgl_lahir`, `no_telp`, `status_perkawinan`, `pendidikan_terakhir`, `surat_lamaran`, `cv`) VALUES
-(2, 'tes 0310 1900', 'jawa timur', 'malang', 'pakis', 'krajan ', 'laki-laki', '18/02/1999', '083898996210', 'belum kawin', 's1', 'Formulir-Surat-Lamaran1.pdf', 'cv2.pdf');
+(448, 'tes 0310 1900', 'jawa timur', 'malang', 'pakis', 'krajan ', 'laki-laki', '18/02/1999', '083898996210', 'belum kawin', 's1', 'Sertifikat5.pdf', 'Tepi_35.pdf');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_karyawan`
+--
+
+CREATE TABLE `tbl_karyawan` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(200) NOT NULL,
+  `provinsi` varchar(100) NOT NULL,
+  `kota_kabupaten` varchar(100) NOT NULL,
+  `kecamatan` varchar(100) NOT NULL,
+  `alamat_lengkap` text NOT NULL,
+  `jk` varchar(15) NOT NULL,
+  `tgl_lahir` varchar(100) NOT NULL,
+  `no_telp` varchar(100) NOT NULL,
+  `status_perkawinan` varchar(100) NOT NULL,
+  `pendidikan_terakhir` varchar(100) NOT NULL,
+  `surat_lamaran` varchar(500) NOT NULL,
+  `cv` varchar(500) NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT 0 COMMENT '1=diterima| 0=pending'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_karyawan`
+--
+
+INSERT INTO `tbl_karyawan` (`id`, `nama`, `provinsi`, `kota_kabupaten`, `kecamatan`, `alamat_lengkap`, `jk`, `tgl_lahir`, `no_telp`, `status_perkawinan`, `pendidikan_terakhir`, `surat_lamaran`, `cv`, `status`) VALUES
+(0, 'tes 0310 1900', 'jawa timur', 'malang', 'pakis', 'krajan ', 'laki-laki', '18/02/1999', '083898996210', 'belum kawin', 's1', 'Sertifikat6.pdf', 'Tepi_36.pdf', 0),
+(0, 'tes 1210', 'jawa timur', 'malang', 'pakis', 'krajan ', 'laki-laki', '18/02/1999', '083898996210', 'belum kawin', 's1', 'Sertifikat7.pdf', 'Tepi_37.pdf', 0),
+(0, 'tes 1210', 'jawa timur', 'malang', 'pakis', 'krajan ', 'laki-laki', '18/02/1999', '083898996210', 'belum kawin', 's1', 'Sertifikat10.pdf', 'Tepi_310.pdf', 0);
 
 -- --------------------------------------------------------
 
@@ -105,7 +137,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `data_lamaran`
 --
 ALTER TABLE `data_lamaran`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=89;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=449;
 
 --
 -- AUTO_INCREMENT for table `users`
