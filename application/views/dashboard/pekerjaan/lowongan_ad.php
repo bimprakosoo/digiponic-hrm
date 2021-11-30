29d28ffdda6c60486cf9785cd093cee4b6d0b0a5
aprildapeter <75058126+aprildapeter@users.noreply.github.com>
2021-11-30 08:13:40 +0700

    admin-karyawan
    admin-karyawan

--- application/views/dashboard/pekerjaan/lowongan_ad.php
+++ application/views/dashboard/pekerjaan/lowongan_ad.php
@@ -27,7 +27,7 @@
         <div class="container-fluid p-0">
 
             <div class="card shadow mb-4 ">
-                <div class="card-header py-3">
+                <div class="card-body py-3" >
                     <!-- <h6 class="m-0 font-weight-bold text-primary">Management User</h6> -->
                     <h1 class="m-0 font-weight-bold ">Daftar Semua Lowongan</h1>
                 </div>
@@ -75,7 +75,7 @@
                                         <td><?= $m['post_date']; ?></td>
                                         <td>
                                             <!-- BEGIN  modal -->
-                                            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#sizedModalLg">
+                                            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#sizedModalLg" >
                                                 Keterangan
                                             </button>
                                             <div class="modal fade" id="sizedModalLg" tabindex="-1" role="dialog" aria-hidden="true">
@@ -148,9 +148,27 @@
                                             </div>
                                             <!-- END  modal -->
                                         </td>
-                                        <td><?= $m['image']; ?></td>
+                                        <td><img src="<?= base_url('assets/image/lowongan/').$m['image']; ?>" alt=""></td>
                                         <td>
-                                            <button class="btn btn-secondary"><i class="fas fa-eye"></i></button>
+                                            <button class="btn btn-secondary" id="set_dtl" data-toggle="modal" data-target="#sizedModalMd"
+                                            
+                                            data-nama_lowongan="<?= $m['nama_lowongan']; ?>" 
+                                            data-lokasi="<?= $m['lokasi']; ?>" 
+                                            data-perusahaan="<?= $m['perusahaan']; ?>" 
+                                            data-industri="<?= $m['industri']; ?>" 
+                                            data-tipe_pekerjaan="<?= $m['tipe_pekerjaan']; ?>" 
+                                            data-pengalaman_kerja="<?= $m['pengalaman_kerja']; ?>" 
+                                            data-insentif_lembur="<?= $m['insentif_lembur']; ?>" 
+                                            data-level_pekerjaan="<?= $m['level_pekerjaan']; ?>" 
+                                            data-pendidikan="<?= $m['pendidikan']; ?>" 
+                                            data-waktu_bekerja="<?= $m['waktu_bekerja']; ?>" 
+                                            data-gaji="<?= $m['gaji']; ?>" 
+                                            data-post_date="<?= $m['post_date']; ?>"
+                                            data-ket="<?= $m['ket']; ?>"
+                                            data-syarat_pengalaman="<?= $m['syarat_pengalaman']; ?>"
+                                            data-tunjangan="<?= $m['tunjangan']; ?>"
+                                            data-image="<?= base_url('assets/image/lowongan/').$m['image']; ?>"
+                                            ><i class="fas fa-eye"></i></button>
                                             <button class="btn btn-primary"><i class="fas fa-edit"></i></button>
                                             <button class="btn btn-danger"><i class="fas fa-trash-alt"></i></button>
                                         </td>
@@ -165,6 +183,95 @@
             </div>
         </div>
     </div>
+
+    <!-- BEGIN  modal -->
+    <div class="modal fade" id="sizedModalMd" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Detail Perusahaan</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body table-responsive ">
+                    <table class="table table-bordered no-margin">
+                        <tbody>
+                            <tr>
+                                <th>Nama Lowongan</th>
+                                <td><span id="dtl_nama"></span></td>
+                            </tr>
+                            <tr>
+                                <th>Lokasi</th>
+                                <td><span id="dtl_lokasi"></span></td>
+                            </tr>
+                            <tr>
+                                <th>Perusahaan</th>
+                                <td><span id="dtl_perusahaan"></span></td>
+                            </tr>
+                            <tr>
+                                <th>Industri</th>
+                                <td><span id="dtl_industri"></span></td>
+                            </tr>
+                            <tr>
+                                <th>Tipe Pekerjaan</th>
+                                <td><span id="dtl_tipe"></span></td>
+                            </tr>
+                            <tr>
+                                <th>Pengalaman Kerja</th>
+                                <td><span id="dtl_pengalaman"></span></td>
+                            </tr>
+                            <tr>
+                                <th>Insentif</th>
+                                <td><span id="dtl_insentif"></span></td>
+                            </tr>
+                            <tr>
+                                <th>Level Kerja</th>
+                                <td><span id="dtl_level"></span></td>
+                            </tr>
+                            <tr>
+                                <th>Pendidikan</th>
+                                <td><span id="dtl_pendidikan"></span></td>
+                            </tr>
+                            <tr>
+                                <th>Waktu Bekerja</th>
+                                <td><span id="dtl_waktu"></span></td>
+                            </tr>
+                            <tr>
+                                <th>Gaji</th>
+                                <td><span id="dtl_gaji"></span></td>
+                            </tr>
+                            <tr>
+                                <th>Post Date</th>
+                                <td><span id="dtl_post"></span></td>
+                            </tr>
+                            <tr>
+                                <th>Keterangan Kerja</th>
+                                <td><span id="dtl_ket"></span></td>
+                            </tr>
+                            <tr>
+                                <th>Syarat Kerja</th>
+                                <td><span id="dtl_syarat"></span></td>
+                            </tr>
+                            <tr>
+                                <th>Tunjangan</th>
+                                <td><span id="dtl_tunjangan"></span></td>
+                            </tr>
+                            <tr>
+                                <th>Image</th>
+                                <td ><img src="" alt="" id="dtl_image"></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- END  modal -->
 </main>
 <!-- End Content -->
 <script>
@@ -172,6 +279,44 @@
         $('#dataTable').DataTable({
             "scrollX": true
 
+        });
+    });
+</script>
+<script type="text/javascript">
+    $(document).ready(function() {
+        $(document).on('click', '#set_dtl', function() {
+            var nama_lowongan = $(this).data('nama_lowongan');
+            var lokasi = $(this).data('lokasi');
+            var perusahaan = $(this).data('perusahaan');
+            var industri = $(this).data('industri');
+            var tipe_pekerjaan = $(this).data('tipe_pekerjaan');
+            var pengalaman_kerja = $(this).data('pengalaman_kerja');
+            var insentif_lembur = $(this).data('insentif_lembur');
+            var level_pekerjaan = $(this).data('level_pekerjaan');
+            var pendidikan = $(this).data('pendidikan');
+            var waktu_bekerja = $(this).data('waktu_bekerja');
+            var gaji = $(this).data('gaji');
+            var post_date = $(this).data('post_date');
+            var ket = $(this).data('ket');
+            var syarat_pengalaman = $(this).data('syarat_pengalaman');
+            var tunjangan = $(this).data('tunjangan');
+            var image = $(this).data('image');
+            $('#dtl_nama').text(nama_lowongan);
+            $('#dtl_lokasi').text(lokasi);
+            $('#dtl_perusahaan').text(perusahaan);
+            $('#dtl_industri').text(industri);
+            $('#dtl_tipe').text(tipe_pekerjaan);
+            $('#dtl_pengalaman').text(pengalaman_kerja);
+            $('#dtl_insentif').text(insentif_lembur);
+            $('#dtl_level').text(level_pekerjaan);
+            $('#dtl_pendidikan').text(pendidikan);
+            $('#dtl_waktu').text(waktu_bekerja);
+            $('#dtl_gaji').text(gaji);
+            $('#dtl_post').text(post_date);
+            $('#dtl_ket').text(ket);
+            $('#dtl_syarat').text(syarat_pengalaman);
+            $('#dtl_tunjangan').text(tunjangan);
+            $('#dtl_image').attr("src", image);
         });
     });
 </script>
---
+++ application/views/karyawan/cuti/v_cuti.php
@@ -0,0 +1,134 @@
+<!-- content -->
+<div class="container-fluid p-0 d-flex align-items-center" style="width: 1184px; height:60px; background: #FCFCFC;">
+    <nav aria-label="breadcrumb" class="m-4">
+        <ol class="breadcrumb bg-transparent p-0 mt-1 mb-0">
+            <li class="breadcrumb-item"><a href="#">Dashboards</a></li>
+            <li class="breadcrumb-item"><a href="#">Kehadiran</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Hak Istimewa</li>
+        </ol>
+    </nav>
+</div>
+<main class="content">
+
+    <div class="container-fluid p-0 ">
+
+        <!-- Daftar Semua Lowongan -->
+        <div class="container-fluid p-0">
+
+            <div class="card ">
+                <div class="row my-2 my-xl-3 m-2">
+                    <div class="col-auto d-none d-sm-block ">
+                        <h3><strong>Izin cuti</strong></h3>
+                    </div>
+
+                    <div class="col-auto ml-auto ">
+                        <button class="btn btn-primary" data-toggle="modal" data-target="#sizedModalMd">Form Izin Sakit</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card shadow mb-4 ">
+                <div class="card-body py-3" style="background: #fff;">
+                    <!-- <h6 class="m-0 font-weight-bold text-primary">Management User</h6> -->
+                    <h3 class="m-0 font-weight-bold">Daftar Data Permohonan Cuti</h3>
+                </div>
+                <div class="card-body ">
+                    <div class="table-responsive">
+                        <table class="table text-center" id="dataTable" width="100%" style="max-width:100%; white-space:nowrap; border: none !important;" cellspacing="0">
+                            <thead>
+                                <tr>
+                                    <th>Nama Karyawan</th>
+                                    <th>Tanggal Pengajuan</th>
+                                    <th>Tanggal Cuti</th>
+                                    <th>Akhir Cuti </th>
+                                    <th>Lama Cuti</th>
+                                    <th>Sisa Cuti</th>
+                                    <th>Keterangan</th>
+                                    <th>Status</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>belum absen</td>
+                                    <td><?php echo date('Y-m-d H:i:s'); ?></td>
+                                    <td><?php echo date('Y-m-d H:i:s'); ?></td>
+                                    <td><?php echo date('Y-m-d H:i:s'); ?></td>
+                                    <td>2</td>
+                                    <td>10</td>
+                                    <td>10</td>
+                                    <td>Ditolak</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <!-- BEGIN  modal -->
+    <div class="modal fade" id="sizedModalMd" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-body">
+                    <h4 class="modal-title">Form Cuti </h4>
+                </div>
+                <div class="modal-body table-responsive ">
+                    <table class="table  no-margin">
+                        <tbody>
+                            <tr>
+                                <td>Tanggal Pengajuan </td>
+                                <td>: </td>
+
+                                <td>
+                                <?php echo date('Y-m-d '); ?>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Tanggal Cuti</td>
+                                <td>:</td>
+
+                                <td><input type="date"></td>
+                            </tr>
+                            <tr>
+                                <td>Sampai Dengan</td>
+                                <td>:</td>
+
+                                <td><input type="date"></td>
+                            </tr>
+                            <tr>
+                                <td>Lama</td>
+                                <td>:</td>
+
+                                <td>3</td>
+                            </tr>
+                            <tr>
+                                <td>Keterangan</td>
+                                <td>:</td>
+
+                                <td><textarea name="ketIjin" id="ketIzin" cols="30" rows="10"></textarea></td>
+                            </tr>
+
+                        </tbody>
+                    </table>
+                </div>
+                <div class="modal-body d-flex justify-content-center">
+                    <button type="button" class="btn btn-primary">Izin Sakit</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- END  modal -->
+
+</main>
+<!-- End Content -->
+<script>
+    $(document).ready(function() {
+        $('#dataTable').DataTable({
+            "scrollX": true,
+            "searching": false
+
+        });
+    });
+</script>
