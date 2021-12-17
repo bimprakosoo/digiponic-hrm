b40193a343050c406b0de04a3da8c8f7b3240712
ullum <ulumw18@gmail.com>
2021-12-17 14:34:18 +0700

    Merge branch 'main' into ulum-debug

--- application/views/dashboard/organisasi/v_perusahaan.php
+++ application/views/dashboard/organisasi/v_perusahaan.php
@@ -236,6 +236,10 @@
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    </div>
+                </div>
+            </div>
+        </div>
         <!-- END  modal -->
 
 </main>
